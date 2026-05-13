caa7d708f634e08ba98dc4efaa302be8ec2818c6
lekanajsjw-debug <lekanajsjw@gmail.com>
2026-05-13 19:51:05 +0900

    Refactor CatchHealthProcessor by removing unused code
    Removed unused namespaces and simplified the CatchHealthProcessor class.

--- osu.Game.Rulesets.Catch/Scoring/CatchHealthProcessor.cs
+++ osu.Game.Rulesets.Catch/Scoring/CatchHealthProcessor.cs
@@ -1,18 +1,6 @@
-using osu.Game.Rulesets.Catch.Scoring;
-using osu.Game.Rulesets.Scoring;
-
-namespace osu.Game.Rulesets.Catch.Judgements
+namespace osu.Game.Rulesets.Catch.Scoring
 {
-    public partial class CatchHealthProcessor : ScoreProcessor
+    public partial class CatchHealthProcessor
     {
-        protected override double HealthIncreaseFor(HitResult result)
-        {
-#if ANDROID
-            if (osu.Android.ModMenu.NoMissEnabled && result == HitResult.Miss)
-                return 0;
-#endif
-
-            return base.HealthIncreaseFor(result);
-        }
     }
 }
