c63400ab4aeef76f567d9bf3db33c2b246f95c42
lekanajsjw <lekanajsjw@gmail.com>
2026-05-13 12:27:05 +0900

    update

---
+++ osu.Game/Configuration/DevelopmentOsuConfigManager.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Platform;
+
+namespace osu.Game.Configuration
+{
+    public class DevelopmentOsuConfigManager : OsuConfigManager
+    {
+        protected override string Filename => base.Filename.Replace(".ini", ".dev.ini");
+
+        public DevelopmentOsuConfigManager(Storage storage, GameHost? host = null)
+            : base(storage, host)
+        {
+        }
+    }
+}
