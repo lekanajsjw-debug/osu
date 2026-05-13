853ee9ece293c74307747a5d6adeb876eb7483ac
lekanajsjw-debug <lekanajsjw@gmail.com>
2026-05-13 19:33:32 +0900

    Add files via upload

---
+++ osu.Android/ModMenuOverlay.cs
@@ -0,0 +1,10 @@
+// Замени старые переключатели на эти:
+
+// Relax
+ModMenu.SetRelax(!ModMenu.RelaxEnabled);
+
+// NoMiss
+ModMenu.SetNoMiss(!ModMenu.NoMissEnabled);
+
+// Hitbox
+ModMenu.SetHitbox(!ModMenu.HitboxEnabled);
